dab2a297e4ab44d75f2194f9aec6be12d31a264d
Eunsung Moon <eunsung9647@gmail.com>
2022-03-21 00:05:41 +0900

    main 파트

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -6,20 +6,20 @@ import throwing from '../Styles/images/throwing.png';
 
 export default function Main() {
   const titles = [
-    { id: '1', name: 'underwater', value: '수중작업', png: underwater},
+    { id: '1', name: 'underwater', value: '수중작업', png: underwater },
     { id: '2', name: 'recovery', value: '폐기물 인양', png: recovery },
     { id: '3', name: 'transportation', value: '폐기물 운반', png: transportation },
-    { id: '4', name: 'throwing', value: '자갈 및 모래 투척',png: throwing }
+    { id: '4', name: 'throwing', value: '자갈 및 모래 투척', png: throwing }
   ]
 
   return (
-    <main id='main'>
-      <h1>조은바다는 수중작업, 인양 및 운반, 투척작업을 하는 전문시공업체 입니다.</h1>
+    <main id='main' className='container'>
+      <h1 className='title'>조은바다는 수중작업, 인양 및 운반, 투척작업을 하는 전문시공업체 입니다.</h1>
       <section className='flex'>
         {
           titles.map((title) => (
-            <div className='box'>
-              <img src={title.png} className={`img-${title.id}`} alt={title.value} />
+            <div className={`box ${title.id === '1' || title.id === '2' ? 'padding15' : ''} ${title.id === '4' ? '' : 'margin52'}`}>
+              <img src={title.png} className={`img-${ title.id }`} alt={title.value} />
               <h2>{title.value}</h2>
             </div>
           ))
