0e10b41184f01b843a44456b6e5f4e12d1831730
Eunsung Moon <eunsung9647@gmail.com>
2022-03-20 13:15:51 +0900

    main 부분 이미지 추가

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import underwater from '../Styles/images/underwater.png';
+import recovery from '../Styles/images/recovery.png';
+import transportation from '../Styles/images/transportation.png';
+import throwing from '../Styles/images/throwing.png';
 
 export default function Main() {
+  const titles = [
+    { id: '1', name: 'underwater', value: '수중작업', png: underwater},
+    { id: '2', name: 'recovery', value: '폐기물 인양', png: recovery },
+    { id: '3', name: 'transportation', value: '폐기물 운반', png: transportation },
+    { id: '4', name: 'throwing', value: '자갈 및 모래 투척',png: throwing }
+  ]
+
   return (
-    <div>zzz</div>
+    <main id='main'>
+      <h1>조은바다는 수중작업, 인양 및 운반, 투척작업을 하는 전문시공업체 입니다.</h1>
+      <section className='flex'>
+        {
+          titles.map((title) => (
+            <div className='box'>
+              <img src={title.png} className={`img-${title.id}`} alt={title.value} />
+              <h2>{title.value}</h2>
+            </div>
+          ))
+        }
+      </section>
+    </main>
   )
 }
