05d0079343241940a0716d964099f36ea4c90aba
Eunsung Moon <eunsung9647@gmail.com>
2022-03-21 00:27:58 +0900

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div className='container-fluid nopadding'>
         <Header />
         <Main />
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </div>
   );
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer id='footer'>
-      <div className='container'>
-        <p>조은바다 전문시공업체 Tel : 010-3390-4411</p>
-        <div>
+    <footer id='footer' className='container-fluid'>
+      <div className='container footerWrap'>
+        <p>조은바다 전문시공업체 상담문의 Tel : 010-3390-4411</p>
+        <div className='flex'>
           <p>조은바다 전문시공업체 위치</p>
-          <p>:전남 여수시 수정동 781</p>
+          <p className='margin-right'>:전남 여수시 수정동 781</p>
         </div>
       </div>
     </footer>
